7d0b9bfe870bc2cf70cad026a818ea7390bb8199
Akshat7garg <akshat7garg@gmail.com>
2025-05-25 01:58:25 +0530

    fix: minor updates and improvements

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import path from "path";
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
@@ -10,18 +11,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (id.includes("react")) return "react-vendor";
-            if (id.includes("motion")) return "motion";
-            if (id.includes("tailwindcss")) return "tailwind";
-            return "vendor";
-          }
-        },
-      },
-    },
-  },
-});
+})
